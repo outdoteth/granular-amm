7cc1cb92b0d1d6d60c74dc42f7b71db5e3bcc37a
outdoteth <outdoteth@users.noreply.github.com>
2022-08-19 09:31:07 +0100

    setup

---
+++ test/Factory.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Factory.sol";
+
+contract FactoryTest is Test {
+
+    function setUp() public {
+    }
+}
