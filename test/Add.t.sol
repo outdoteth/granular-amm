9b4fd8989a127439cc4b6a80b3521ab871a05050
outdoteth <outdoteth@users.noreply.github.com>
2022-08-19 17:50:45 +0100

    add add test

--- test/Add.t.sol
+++ test/Add.t.sol
@@ -2,20 +2,40 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 
 import "../src/Factory.sol";
 import "../src/Pool.sol";
 import "./Fixture.sol";
 
+
 contract AddTest is Fixture {
-    function setUp() public {}
+    Pool public p;
+
+    function setUp() public {
+        bytes32 root = m.getRoot(tokenIdData);
+        bytes32[] memory merkleRoots = new bytes32[](1);
+        merkleRoots[0] = root;
+        
+        p = f.create(address(bayc), merkleRoots);
+        bayc.setApprovalForAll(address(p), true);
+    }
 
     function testItInitsLiquidity() public {
         Pool.LpAdd[] memory lpAdds = new Pool.LpAdd[](1);
         Pool.SubpoolToken[] memory subpoolTokens= new Pool.SubpoolToken[](1);
+       
+        subpoolTokens[0] = Pool.SubpoolToken({
+            tokenId: 1,
+            proof: m.getProof(tokenIdData, 0)
+        });
 
         lpAdds[0] = Pool.LpAdd({
-
+            tokens: subpoolTokens,
+            subpoolId: 0,
+            ethAmount: 1 ether
         });
+
+        p.add{value: 1 ether}(lpAdds);
     }
 }
--- test/Fixture.sol
+++ test/Fixture.sol
@@ -2,15 +2,30 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import {Merkle} from "murky/Merkle.sol";
+
 import "../src/Factory.sol";
 import "./MockERC721.sol";
 
 contract Fixture is Test {
     Factory public f;
     MockERC721 public bayc;
+    Merkle public m;
+    bytes32[] public tokenIdData;
 
     constructor() {
         f = new Factory();
         bayc = new MockERC721("fake bayc", "fbayc");
+
+        m = new Merkle();
+        tokenIdData.push(bytes32(uint256(1)));
+        tokenIdData.push(bytes32(uint256(2)));
+        tokenIdData.push(bytes32(uint256(3)));
+        tokenIdData.push(bytes32(uint256(4)));
+
+        bayc.mint(address(this), 1);
+        bayc.mint(address(this), 2);
+        bayc.mint(address(this), 3);
+        bayc.mint(address(this), 4);
     }
 }
